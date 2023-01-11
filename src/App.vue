63ebe6caafbac52296c02c3546ec2bb93e407406
takahashikosuke <takahashikosuke@team-lab.com>
2023-01-11 21:37:19 +0900

    :wrench: 一旦App.vueを修正

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
 <script setup lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue";
 import { useI18n } from "vue-i18n";
 
 const { t } = useI18n();
 </script>
 
 <template>
-  <RouterView />
+  <div class="h-full bg-gray-50 dark:bg-gray-800">
+    <header>
+      <p>resume</p>
+    </header>
+    <div class="flex">
+      <aside class="w-64" aria-label="Sidebar">
+        <div class="px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+          <ul class="space-y-2">
+            <li>
+              <router-link
+                to="/"
+                class="p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >HOME</router-link
+              >
+            </li>
+            <li>
+              <router-link
+                to="/works"
+                class="p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >WORKS</router-link
+              >
+            </li>
+          </ul>
+        </div>
+      </aside>
+      <main class="bg-gray-50 dark:bg-gray-800">
+        <RouterView />
+      </main>
+    </div>
+  </div>
 </template>
 
 <style scoped>
