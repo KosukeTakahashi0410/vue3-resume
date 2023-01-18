a6b1e87aa9eb72057e4c27b0017df6b3f0ba9cef
takahashikosuke <takahashikosuke@team-lab.com>
2023-01-18 22:08:34 +0900

    :wrench: 言語切り替えを追加

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,40 @@
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
 import { routes } from "@/router";
+const { t, locale } = useI18n();
 
-const { t } = useI18n();
+/** 言語切り替え */
+const changeLanguage = () => {
+  switch (locale.value) {
+    case "ja":
+      locale.value = "en";
+      return;
+    default:
+      locale.value = "ja";
+  }
+};
 </script>
 
 <template>
   <div class="h-screen bg-gray-50 dark:bg-gray-800">
-    <header class="flex justify-between p-5">
-      <!-- TODO ここを画像なりアイコンにしたい -->
-      <p class="dark:text-white">{{ t("header.title") }}</p>
+    <header class="flex justify-between items-center p-5 sticky top-0 z-50">
+      <div class="flex justify-between items-center">
+        <!-- TODO アイコンに -->
+        <p class="dark:text-white pr-1">{{ t("header.title") }}</p>
+        <!-- TODO アイコンに -->
+        <button type="button" @click="changeLanguage" class="dark:text-white">
+          言語変更
+        </button>
+      </div>
       <ul class="md:flex hidden gap-x-2">
         <li
           v-for="(route, index) in routes"
           :key="index"
-          class="dark:text-white"
+          class="dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 p-1.5 rounded-lg"
         >
-          <a :href="route.path">{{ t(`header.link.${route.name}`) }}</a>
+          <router-link :to="route.path">
+            {{ t(`header.link.${route.name}`) }}
+          </router-link>
         </li>
       </ul>
     </header>
@@ -29,14 +47,14 @@ const { t } = useI18n();
               <router-link
                 to="/"
                 class="p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                >{{ t("sideBar.top") }}</router-link
+                >{{ "わろた" }}</router-link
               >
             </li>
             <li>
               <router-link
                 to="/works"
                 class="p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                >{{ t("sideBar.work") }}</router-link
+                >{{ "わろた" }}</router-link
               >
             </li>
           </ul>
