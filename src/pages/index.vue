461f925900a454770efc1cbc2387598e2e3a1123
takahashikosuke <takahashikosuke@team-lab.com>
2023-01-19 20:38:37 +0900

    :wrench: 不要な内容を削除

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue";
 import { useI18n } from "vue-i18n";
 
 const { t } = useI18n();
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
   <p>{{ t("test.hello") }}</p>
 </template>
