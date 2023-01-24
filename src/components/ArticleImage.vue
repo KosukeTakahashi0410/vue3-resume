266a35266bd27d3751a3dcb1a75337e3be5570e5
takahashikosuke <takahashikosuke@team-lab.com>
2023-01-24 21:21:36 +0900

    :sparkles: imageのコンポーネントを追加

---
+++ src/components/ArticleImage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+type Props = {
+  imagePath: string;
+};
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <!-- FIXME: VueImageみたいに最適化できないかを検討 -->
+  <img :src="props.imagePath" alt="" />
+</template>
