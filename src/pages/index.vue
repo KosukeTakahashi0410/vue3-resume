0064a448f40e74fca921d0c784d809b0a88b3d37
takahashikosuke <takahashikosuke@team-lab.com>
2023-01-25 20:35:54 +0900

    :wrench: 画像を出す

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
+import ArticleImage from "@/components/ArticleImage.vue";
+import Photo1 from "@/assets/images/photo1.png";
 
 const { t } = useI18n();
 </script>
 
 <template>
   <p>{{ t("test.hello") }}</p>
+  <ArticleImage :imageSrc="Photo1" />
+  <p>a</p>
 </template>
