cac2d848d4753688d7deebf8fbc9cb93decc05ab
takahashikosuke <takahashikosuke@team-lab.com>
2023-01-26 21:13:02 +0900

    :wrench: wrapperを追加

---
+++ src/components/ArticleWrapper.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+type Props = {
+  imageSrc: string;
+};
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <!-- ここをwrapperにする -->
+  <div>
+    <slot></slot>
+  </div>
+</template>
