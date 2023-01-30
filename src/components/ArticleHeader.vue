0b006871bb77f9fbdeca55b001791961a96bc6db
takahashikosuke <takahashikosuke@team-lab.com>
2023-01-30 22:13:53 +0900

    :wrench: hash化対応

--- src/components/ArticleHeader.vue
+++ src/components/ArticleHeader.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
+import { getHashedLink } from "@/utils/url";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
 type Props = {
   text: string;
   variant: "h1" | "h2" | "h3";
+  hash?: string;
 };
 
 const props = defineProps<Props>();
 </script>
 
 <template>
-  <h1 v-if="props.variant === 'h1'">{{ props.text }}</h1>
-  <h2 v-else-if="props.variant === 'h2'">{{ props.text }}</h2>
-  <h3 v-else>{{ props.text }}</h3>
+  <router-link v-if="hash" :to="getHashedLink(route.path, hash)">
+    <h1 v-if="props.variant === 'h1'">{{ props.text }}</h1>
+    <h2 v-else-if="props.variant === 'h2'">{{ props.text }}</h2>
+    <h3 v-else>{{ props.text }}</h3>
+  </router-link>
+  <template v-else>
+    <h1 v-if="props.variant === 'h1'">{{ props.text }}</h1>
+    <h2 v-else-if="props.variant === 'h2'">{{ props.text }}</h2>
+    <h3 v-else>{{ props.text }}</h3>
+  </template>
 </template>
