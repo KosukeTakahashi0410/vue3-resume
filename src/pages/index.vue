439a91e63a57cf1681ff772cf1fad93c4d5b23c8
takahashikosuke <takahashikosuke@team-lab.com>
2023-02-06 20:59:59 +0900

    :wrench: セクションで区分け

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,30 +10,39 @@ const { t } = useI18n();
 </script>
 
 <template>
-  <!-- fixme: article wrapper にflexとflex gapをつける -->
   <ArticleWrapper>
-    <ArticleHeader :text="t('home.title')" variant="h1" />
-    <ArticleHeader :text="t('home.profile.title')" variant="h2" />
-    <ArticleImage
-      :imageSrc="Photo2"
-      :description="t('home.photo.description')"
-    />
-    <ArticleBody :body="t('home.profile.body')" />
-    <ArticleHeader :text="t('home.career.title')" variant="h2" />
-    <ArticleHeader :text="t('home.career.teamLab.title')" variant="h3" />
-    <ArticleBody :body="t('home.career.teamLab.body')" />
-    <ArticleHeader
-      :text="t('home.career.teamLabEngineering.title')"
-      variant="h3"
-    />
-    <ArticleBody :body="t('home.career.teamLabEngineering.body')" />
-    <ArticleHeader :text="t('home.career.jTips.title')" variant="h3" />
-    <ArticleBody :body="t('home.career.jTips.body')" />
-    <ArticleHeader :text="t('home.educationalBackground.title')" variant="h2" />
-    <ArticleHeader
-      :text="t('home.educationalBackground.university.title')"
-      variant="h3"
-    />
-    <ArticleBody :body="t('home.educationalBackground.university.body')" />
+    <!-- fixme: section wrapperを作る childrenを渡すイメージ -->
+    <div>
+      <ArticleHeader :text="t('home.title')" variant="h1" />
+      <ArticleHeader :text="t('home.profile.title')" variant="h2" />
+      <ArticleImage
+        :imageSrc="Photo2"
+        :description="t('home.photo.description')"
+      />
+      <ArticleBody :body="t('home.profile.body')" />
+    </div>
+    <div>
+      <ArticleHeader :text="t('home.career.title')" variant="h2" />
+      <ArticleHeader :text="t('home.career.teamLab.title')" variant="h3" />
+      <ArticleBody :body="t('home.career.teamLab.body')" />
+      <ArticleHeader
+        :text="t('home.career.teamLabEngineering.title')"
+        variant="h3"
+      />
+      <ArticleBody :body="t('home.career.teamLabEngineering.body')" />
+      <ArticleHeader :text="t('home.career.jTips.title')" variant="h3" />
+      <ArticleBody :body="t('home.career.jTips.body')" />
+    </div>
+    <div>
+      <ArticleHeader
+        :text="t('home.educationalBackground.title')"
+        variant="h2"
+      />
+      <ArticleHeader
+        :text="t('home.educationalBackground.university.title')"
+        variant="h3"
+      />
+      <ArticleBody :body="t('home.educationalBackground.university.body')" />
+    </div>
   </ArticleWrapper>
 </template>
