941e3a564b2592a7b2c70af0fc91fdc86aceace0
takahashikosuke <takahashikosuke@team-lab.com>
2023-02-15 22:20:39 +0900

    :wrench: workのベース

--- src/pages/works.vue
+++ src/pages/works.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue";
 import { useI18n } from "vue-i18n";
+import ArticleImage from "@/components/ArticleImage.vue";
+import ArticleWrapper from "@/components/ArticleWrapper.vue";
+import ArticleHeader from "@/components/ArticleHeader.vue";
+import ArticleBody from "@/components/ArticleBody.vue";
+import ArticleSection from "@/components/ArticleSection.vue";
+import Photo2 from "@/assets/images/photo2.png";
 
 const { t } = useI18n();
 </script>
 
 <template>
-  <div>
-    <p>this is works page</p>
-    <p>{{ t("test.hello") }}</p>
-  </div>
+  <ArticleWrapper>
+    <ArticleSection>
+      <ArticleHeader :text="test" variant="h1" />
+      <ArticleImage
+        :imageSrc="Photo2"
+        :description="t('home.photo.description')"
+      />
+      <ArticleBody :body="test" />
+    </ArticleSection>
+  </ArticleWrapper>
 </template>
-
-<style scoped></style>
